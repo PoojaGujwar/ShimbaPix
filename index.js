7d4eef9c28d9065af39c74e9c875ad5ea9788f7f
unknown <gujwarpooja@gmail.com>
2025-05-26 16:04:32 +0530

    update : auth uri

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const upload = multer({ storage });
 
 
 app.get("/auth/google", (req, res) => {
-  const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:${PORT}/auth/google/callback&response_type=code&scope=profile email`;
+  const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=https://shimbapix.onrender.com/auth/google/callback&response_type=code&scope=profile email`;
   res.redirect(googleAuthUrl);
 });
 
