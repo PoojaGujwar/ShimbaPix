c9ac7ff98dc869810750c9c22c7553ad691d8766
unknown <gujwarpooja@gmail.com>
2025-05-26 22:17:01 +0530

    change in origin

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ io.emit("receive_album",data)
 
 dotenv.config();
 
-app.use(cors({ credentials: true, origin:["http://localhost:3000", process.env.FRONTEND_URL]}));
+app.use(cors({ credentials: true, origin:["http://localhost:3000", "https://shimbapix.onrender.com"]}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json());
