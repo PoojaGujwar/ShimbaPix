62390cfa2a70928c96c83a348d2b3dcf1270fedd
unknown <gujwarpooja@gmail.com>
2025-05-27 10:55:44 +0530

    fect: url change

--- index.js
+++ index.js
@@ -13,51 +13,54 @@ const { setSecureCookie } = require("./services/index.js");
 const Album = require("./models/Albums.model.js");
 const ImageV2 = require("./models/Images.model.js");
 const UserModel = require("./models/user.model.js");
-const ShareData = require("./models/Shares.model.js")
+const ShareData = require("./models/Shares.model.js");
 const PORT = 4000;
-const http = require('http')
-const {Server} = require('socket.io')
-const server=http.createServer(app)
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
 
-const io=new Server(server,{
-  cors:{
-    origin:"https://simba-pix-frontend.vercel.app",  methods: ["GET", "POST"],
-    credentials: true
-  }
-})
-io.on("connection",(socket)=>{
-  console.log("User Connected",socket.id)
-  socket.on('send_album',async(data)=>{
-    console.log(data,"DATA")
-    const {sender,receiver,album} = data
+const io = new Server(server, {
+  cors: {
+    origin: "https://simba-pix-frontend.vercel.app",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+io.on("connection", (socket) => {
+  console.log("User Connected", socket.id);
+  socket.on("send_album", async (data) => {
+    console.log(data, "DATA");
+    const { sender, receiver, album } = data;
     const existingShare = await ShareData.findOne({ sender, receiver, album });
     if (existingShare) {
-    console.log("Album already shared with this user.");
-    
-   //message send already send
-    socket.emit("receive_album", {
-      success: false,
-      message: "Album already shared with this user.",
-      data: null
-    });
-    return;
-  }
-    const newShared = new ShareData({sender,receiver,album})
-    await newShared.save()
-io.emit("receive_album",data)
-  })
-  socket.on("disconnect",()=>{
-    console.log("User Disconnected",socket.io)
-  })
-})
+      console.log("Album already shared with this user.");
 
-
+      //message send already send
+      socket.emit("receive_album", {
+        success: false,
+        message: "Album already shared with this user.",
+        data: null,
+      });
+      return;
+    }
+    const newShared = new ShareData({ sender, receiver, album });
+    await newShared.save();
+    io.emit("receive_album", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.io);
+  });
+});
 
 dotenv.config();
 
-app.use(cors({  origin:"https://simba-pix-frontend.vercel.app", 
+app.use(
+  cors({
+    origin: "https://simba-pix-frontend.vercel.app",
     methods: ["GET", "POST"],
-    credentials: true}));
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json());
@@ -71,7 +74,6 @@ cloudinary.config({
 });
 const storage = multer.diskStorage({});
 const upload = multer({ storage });
-
 
 app.get("/auth/google", (req, res) => {
   const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=https://shimbapix.onrender.com/auth/google/callback&response_type=code&scope=profile email`;
@@ -103,7 +105,7 @@ app.get("/auth/google/callback", async (req, res) => {
     );
     accessToken = tokenResponse.data.access_token;
     setSecureCookie(res, accessToken);
-    return res.redirect(`https://simba-pix-frontend.vercel.app/v2/profile/google`);
+    return res.redirect(`${process.env.FRONTEND_URL}/v2/profile/google`);
   } catch (error) {
     console.error(error);
   }
@@ -111,7 +113,7 @@ app.get("/auth/google/callback", async (req, res) => {
 app.get("/user/profile/google", verifyAccessToken, async (req, res) => {
   try {
     const { access_token } = req.cookies;
-    console.log(access_token)
+    console.log(access_token);
     const googleUserDataResponse = await axios.get(
       "https://www.googleapis.com/oauth2/v2/userinfo",
       {
@@ -152,7 +154,7 @@ app.post("/albums", async (req, res) => {
   }
 });
 
-app.get("/albums",verifyAccessToken, async (req, res) => {
+app.get("/albums", verifyAccessToken, async (req, res) => {
   try {
     const allAlbums = await Album.find();
     res.status(200).json(allAlbums);
@@ -163,13 +165,17 @@ app.get("/albums",verifyAccessToken, async (req, res) => {
 });
 app.post("/albums/:id/share", async (req, res) => {
   const albumId = req.params.id;
-  const {sharedUser} = req.body;
+  const { sharedUser } = req.body;
   try {
-    const updatedAlbum = await Album.findByIdAndUpdate(albumId, {
-    $addToSet: { sharedUser: sharedUser },
-  }, {
-      new: true,
-    });
+    const updatedAlbum = await Album.findByIdAndUpdate(
+      albumId,
+      {
+        $addToSet: { sharedUser: sharedUser },
+      },
+      {
+        new: true,
+      }
+    );
     res
       .status(202)
       .json({ message: "Share album successfully", albums: updatedAlbum });
@@ -239,7 +245,7 @@ app.post("/images", upload.single("image"), async (req, res) => {
   }
 });
 
-app.get("/images", verifyAccessToken,async (req, res) => {
+app.get("/images", verifyAccessToken, async (req, res) => {
   try {
     const images = await ImageV2.find();
     res.status(200).json(images);
@@ -257,9 +263,9 @@ app.delete("/images/:id", async (req, res) => {
   }
 });
 app.get("/v1/users", async (req, res) => {
-  const {currentEmail} = req.query
+  const { currentEmail } = req.query;
   try {
-    const users = await UserModel.find({email: {$ne:currentEmail}});
+    const users = await UserModel.find({ email: { $ne: currentEmail } });
     res.status(200).json(users);
   } catch (error) {
     res
@@ -268,23 +274,27 @@ app.get("/v1/users", async (req, res) => {
   }
 });
 
-app.get("/v1/shareData",async(req,res)=>{
-  try{
-    const data = await ShareData.find().populate("album")
-    res.status(200).json(data)
-  }catch(error){
-    res.status(500).json({message:"Error while fetching share album",error:error})
+app.get("/v1/shareData", async (req, res) => {
+  try {
+    const data = await ShareData.find().populate("album");
+    res.status(200).json(data);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error while fetching share album", error: error });
   }
-})
-app.delete("/v1/shareData/:id",async(req,res)=>{
-  const albumId = req.params.id
-  try{
-    const allData = await ShareData.deleteMany({album:albumId})
-    res.status(200).json({message:"Share data deleted",data:allData})
-  }catch(error){
-    res.status(500).json({message:"Error while delete share album",error:error})
+});
+app.delete("/v1/shareData/:id", async (req, res) => {
+  const albumId = req.params.id;
+  try {
+    const allData = await ShareData.deleteMany({ album: albumId });
+    res.status(200).json({ message: "Share data deleted", data: allData });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error while delete share album", error: error });
   }
-})
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
