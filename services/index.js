6c55fd8bd49b9196d4543741846b7bef7692fd94
unknown <gujwarpooja@gmail.com>
2025-05-27 22:48:03 +0530

    fect adding cookie

--- services/index.js
+++ services/index.js
@@ -1,7 +1,9 @@
 function setSecureCookie(res,token){
     res.cookie("access_token",token,{
-        httpOnly:false,
-        maxAge:24*60*60*1000,
+     httpOnly: true,        
+    secure: true,          
+    sameSite: 'none',      
+    maxAge: 24 * 60 * 60 * 1000,  
     });
     
     return res;
