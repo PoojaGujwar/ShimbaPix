7156cb5984797d6ef1f2d49e561f9b594fb645c6
unknown <gujwarpooja@gmail.com>
2025-05-30 13:07:14 +0530

    .

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://simba-pix-av1bt07wn-pooja-gujwars-projects.vercel.app",
+    origin: "https://simba-pix-ui.vercel.app/",
      credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
@@ -36,7 +36,7 @@ app.use(
 
 const io = new Server(server, {
   cors: {
-    origin: "https://simba-pix-av1bt07wn-pooja-gujwars-projects.vercel.app",
+    origin: "https://simba-pix-ui.vercel.app",
     credentials:true
   },
 });
