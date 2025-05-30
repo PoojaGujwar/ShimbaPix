7ba740264c37da8dc3c01d2ad59ba2d04944bcbb
unknown <gujwarpooja@gmail.com>
2025-05-30 19:43:28 +0530

    change in Cookie setting

--- services/index.js
+++ services/index.js
@@ -1,12 +1,10 @@
 function setSecureCookie(res,token){
     res.cookie("access_token",token,{
         httpOnly:true,
-        secure:true,
         secure: true, 
         sameSite: 'None',
         maxAge:60*1000,
     });
-    return res;
 }
 module.exports = {setSecureCookie}
 
